9b8bf41db793d5b0de3aeac7ab76cdde67a513ac
Max <mczonk@gmail.com>
2014-07-07 07:15:26 +0200

    Initial Commit

---
+++ MetalPlus/MetalPlus.h
@@ -0,0 +1,13 @@
+//
+//  MetalPlus.h
+//  MetalPlus
+//
+//  Created by Maximilian Christ on 07/07/14.
+//  Copyright (c) 2014 McZonk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MetalPlus : NSObject
+
+@end
---
+++ MetalPlus/MetalPlus.m
@@ -0,0 +1,13 @@
+//
+//  MetalPlus.m
+//  MetalPlus
+//
+//  Created by Maximilian Christ on 07/07/14.
+//  Copyright (c) 2014 McZonk. All rights reserved.
+//
+
+#import "MetalPlus.h"
+
+@implementation MetalPlus
+
+@end
